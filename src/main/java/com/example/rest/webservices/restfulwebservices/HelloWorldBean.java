2cc233639ddd54a25eaab9a42e664909e03839c8
Konstantinos <kongallis@gmail.com>
2020-07-23 16:30:39 +0300

    Created a REST controller which uses a Pathvariable

--- src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldBean.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -17,22 +17,6 @@ public class HelloWorldBean {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof HelloWorldBean)) {
-            return false;
-        }
-        HelloWorldBean helloWorldBean = (HelloWorldBean) o;
-        ret;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCod);
-    }
-
-    @Override
     public String toString() {
         return "{" + "}";
     }
